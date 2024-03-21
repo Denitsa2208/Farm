68bfb71bfc0007565f45a0846674c4e68c45fc12
Denitsa2208 <162876131+Denitsa2208@users.noreply.github.com>
2024-03-21 19:33:01 +0200

    Add files via upload

---
+++ FarmQuery.sql
@@ -0,0 +1,48 @@
+﻿CREATE TABLE [dbo].[Animals]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [animal_type] VARCHAR NULL, 
+    [production] VARCHAR NULL, 
+    [animals_number] INT NULL
+)
+CREATE TABLE [dbo].[cows]
+(
+	[cow_id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [date_added] DATE NOT NULL, 
+    [number] INT NOT NULL, 
+    [milk_production ] FLOAT NOT NULL, 
+    [production_date] DATE NOT NULL,
+    FOREIGN KEY (cow_id) REFERENCES Animals(Id)
+)
+CREATE TABLE [dbo].[goats]
+(
+	[goat_id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [date_added] DATE NOT NULL, 
+    [number] INT NOT NULL, 
+    [milk_production] FLOAT NOT NULL,
+    [production_date] DATE NOT NULL, 
+    FOREIGN KEY (goat_id) REFERENCES Animals(Id)
+    
+
+)
+CREATE TABLE [dbo].[chickens]
+(
+	[chicken_id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [date_added] DATE NULL,  
+    [eggs_production] FLOAT NULL, 
+    [production_date] DATE NULL,
+    FOREIGN KEY (chicken_id) REFERENCES Animals(Id)
+)
+CREATE TABLE [dbo].[chickens]
+(
+	[chicken_id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [date_added] DATE NULL,  
+    [eggs_production] FLOAT NULL, 
+    [production_date] DATE NULL,
+    FOREIGN KEY (chicken_id) REFERENCES Animals(Id)
+)
+CREATE TABLE [dbo].[Products]
+(
+	[product] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [quantity] FLOAT NULL
+)
